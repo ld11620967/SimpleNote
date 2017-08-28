1a75b1ba880781a67c7fbbb61d2dc91407911c6e
nilin <11620967@qq.com>
2017-08-28 18:07:43 +0800

    Add edit button

--- app/src/main/java/com/nilin/simplenote/DetailsActivity.kt
+++ app/src/main/java/com/nilin/simplenote/DetailsActivity.kt
@@ -3,6 +3,9 @@ package com.nilin.developgoods
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_details.*
 import com.nilin.simplenote.*
 
@@ -10,22 +13,43 @@ import com.nilin.simplenote.*
 class DetailsActivity : AppCompatActivity() {
     var note: String? = null
     var ss: String? = null
-    var id:Long?=null
+    var id: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        setSupportActionBar(toolbar_details)
 
-        var intent:Intent= getIntent()
-        id=intent.getStringExtra("id").toLong()
-        ss=intent.getStringExtra("note")
+        var intent: Intent = getIntent()
+        id = intent.getStringExtra("id").toLong()
+        ss = intent.getStringExtra("note")
         note_details.setText(ss)
         note_details.setSelection(note_details.getText().length)
     }
 
     override fun onBackPressed() {
-            note = note_details.text.toString()
-            val contrastnote = note_details.text.toString().trim()
+        edit()
+        super.onBackPressed()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_edit, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_edit -> {
+                edit()
+                true
+            }
+            else ->super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun edit() {
+        note = note_details.text.toString()
+        val contrastnote = note_details.text.toString().trim()
         if (contrastnote != "") {
             val list = Note(id, note)
             App.instance.getNoteDao().update(list)
@@ -39,6 +63,5 @@ class DetailsActivity : AppCompatActivity() {
             startActivity(Intent)
             finish()
         }
-        super.onBackPressed()
     }
 }
--- app/src/main/java/com/nilin/simplenote/NewActivity.kt
+++ app/src/main/java/com/nilin/simplenote/NewActivity.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_details.*
 import com.nilin.simplenote.*
 
@@ -16,12 +19,41 @@ class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        setSupportActionBar(toolbar_details)
+
         Handler().postDelayed({ showInputMethod() }, 100)
     }
 
     override fun onBackPressed() {
+        edit()
+        super.onBackPressed()
+    }
 
-            note = note_details.text.toString()
+    private fun showInputMethod() {
+        //自动弹出键盘
+        val inputManager = baseContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+        //强制隐藏Android输入法窗口
+        // inputManager.hideSoftInputFromWindow(edit.getWindowToken(),0);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_edit, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_edit -> {
+                edit()
+                true
+            }
+            else ->super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun edit() {
+        note = note_details.text.toString()
         if (note != "") {
             val list = Note(null, note)
             App.instance.getNoteDao().insertInTx(list)
@@ -33,14 +65,6 @@ class NewActivity : AppCompatActivity() {
             startActivity(Intent)
             finish()
         }
-        super.onBackPressed()
     }
 
-    private fun showInputMethod() {
-        //自动弹出键盘
-        val inputManager = baseContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
-        //强制隐藏Android输入法窗口
-        // inputManager.hideSoftInputFromWindow(edit.getWindowToken(),0);
-    }
 }
